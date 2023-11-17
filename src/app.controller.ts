1fc57feb161c81b6899718f08e5470bdfb7163d6
aaron67 <aaron67@aaron67.cc>
2023-11-18 04:59:20 +0800

    Add extra signed support when getting chain info

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,8 +1,15 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  HttpException,
+  HttpStatus,
+  Param,
+  Query,
+} from '@nestjs/common';
 import { AppService } from './app.service';
 import { RabinService } from './rabin/rabin.service';
 import { getTimestamp, toBufferLE } from './utils';
-import { ApiOperation, ApiParam, ApiTags } from '@nestjs/swagger';
+import { ApiOperation, ApiParam, ApiQuery, ApiTags } from '@nestjs/swagger';
 
 @Controller()
 export class AppController {
@@ -98,20 +105,60 @@ export class AppController {
     type: String,
     description: 'chain name, case insensitive, e.g. BSV',
   })
-  async getChainInfo(@Param('chain') chain: string) {
+  @ApiQuery({
+    name: 'extra',
+    required: false,
+    type: String,
+    description: 'extra signed fields: bestBlockHash,chainWork,medianTimePast',
+  })
+  async getChainInfo(
+    @Param('chain') chain: string,
+    @Query('extra') extra: string,
+  ) {
+    const optionalFields = ['bestblockhash', 'chainwork', 'mediantimepast'];
+    const extraFields = extra
+      ? extra.split(',').map((e) => e.trim().toLowerCase())
+      : [];
+    const unknownFields = extraFields.filter(
+      (e) => !optionalFields.includes(e),
+    );
+    if (unknownFields.length > 0) {
+      throw new HttpException(
+        `unknown extra signed fields`,
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+    // a marker that represents the extra signed fields
+    // e.g. the index of `chainwork` in `optionalFields` is 1
+    //   if extraFields includes `chainwork`
+    //   then `extraMarker` & 00000010 would be 1
+    let extraMarker = 0;
+    extraFields.forEach((e) => (extraMarker |= 2 ** optionalFields.indexOf(e)));
+
     chain = chain.toUpperCase();
     const chainInfo = await this.appService.getChainInfo(chain);
     const height = chainInfo.blocks;
-    const medianTimePast = chainInfo.mediantime;
     const bestBlockHash = chainInfo.bestblockhash;
+    const chainWork = chainInfo.chainwork;
+    const medianTimePast = chainInfo.mediantime;
+
+    // values in the same order as their keys in `optionalFields`
+    const optionalSigned = [
+      Buffer.from(bestBlockHash, 'hex'), // best block hash, 32 bytes
+      Buffer.from(chainWork, 'hex'), // chain work, 32 bytes
+      toBufferLE(medianTimePast, 4), // median time past, 4 bytes LE
+    ];
+    const extraSigned = optionalFields.map((e, i) =>
+      extraFields.includes(e) ? optionalSigned[i] : Buffer.alloc(0),
+    );
 
     const timestamp = getTimestamp();
     const data = Buffer.concat([
       toBufferLE(AppController.MARKER.CHAININFO, 1), // api marker, 1 byte
       toBufferLE(timestamp, 4), // timestamp, 4 bytes LE
       toBufferLE(height, 4), // block height, 4 bytes LE
-      toBufferLE(medianTimePast, 4), // medium time past, 4 bytes LE
-      Buffer.from(bestBlockHash, 'hex'), // best block hash, 32 bytes
+      toBufferLE(extraMarker, 1), // extra signed marker, 1 byte
+      ...extraSigned,
       Buffer.from(chain), // chain name
     ]);
     const sigResponse = this.rabinService.sign(data);
@@ -121,7 +168,9 @@ export class AppController {
       chain,
       height,
       bestBlockHash,
+      chainWork,
       medianTimePast,
+      extraSignedMarker: extraMarker,
       ...sigResponse,
     };
   }
