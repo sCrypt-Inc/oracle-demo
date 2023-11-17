c06d623fc4d7f51218e4ecd540d192f5d559585b
aaron67 <aaron67@aaron67.cc>
2023-11-17 14:56:35 +0800

    Fix Swagger

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,15 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const config = new DocumentBuilder().setTitle('Oracle Demo Document').build();
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('docs', app, document);
+  SwaggerModule.setup('docs', app, document, {
+    // https://stackoverflow.com/a/76095075
+    customCssUrl:
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/5.0.0/swagger-ui.min.css',
+    customJs: [
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/5.0.0/swagger-ui-bundle.js',
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/5.0.0/swagger-ui-standalone-preset.js',
+    ],
+  });
   await app.listen(3000);
 }
 bootstrap();
