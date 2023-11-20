4a6fa7bc3b6ae1c26f83f8a9d12e3841e08300c4
aaron67 <aaron67@aaron67.cc>
2023-11-21 05:50:23 +0800

    Add a demo

--- README.md
+++ README.md
@@ -1,5 +1,12 @@
 # oracle-demo
 
+Swagger runs at http://localhost:3000/docs
+
+Demo of how the smart contract [PriceBet](./src/contracts/priceBet.ts) uses this Oracle
+
+- [Code example](./src/app.controller.ts#L44-L102)
+- Trigger contract deploy and call: http://localhost:3000/demo
+
 Vercel deployment: https://oracle-demo.vercel.app/docs
 
 ## Installation
@@ -10,12 +17,19 @@ $ npm install
 
 ## Running the app
 
-Generate your own Rabin private key then copy the output of the below command to a `.env` file under the project root directory.
+**Step 1**. Create a `.env` file under the project root directory from template `.env.example`.
+
+Generate `RABIN_PRIV_P` and `RABIN_PRIV_Q` with command `npm run genRabinPrivKey`.
+
+![](https://aaron67-public.oss-cn-beijing.aliyuncs.com/202311210711000.png)
+
+Generate `PRIVATE_KEY` with command `npm run genBsvPrivKey`.
+
+![](https://aaron67-public.oss-cn-beijing.aliyuncs.com/202311210709519.png)
+
+**Step 2**. Run this app with the following commands.
 
 ```bash
-# generate private key
-$ npm run genPrivKey
-
 # development
 $ npm run start
 
@@ -25,8 +39,6 @@ $ npm run start:dev
 # production mode
 $ npm run start:prod
 ```
-
-Swagger runs at http://localhost:3000/docs.
 
 ## Test
 
--- src/app.controller.ts
+++ src/app.controller.ts
@@ -2,6 +2,20 @@ import { Controller, Get } from '@nestjs/common';
 import { RabinService } from './rabin/rabin.service';
 import { toBufferLE } from './utils';
 import { ApiOperation, ApiTags } from '@nestjs/swagger';
+import { PriceBet } from './contracts/priceBet';
+import {
+  toByteString,
+  bsv,
+  toHex,
+  PubKey,
+  TestWallet,
+  DefaultProvider,
+  findSig,
+  MethodCallOptions,
+} from 'scrypt-ts';
+import axios from 'axios';
+import { myPrivateKey, myPublicKey } from './helpers/bsvPrivKey';
+import artifact from '../artifacts/priceBet.json';
 
 @Controller()
 export class AppController {
@@ -20,6 +34,70 @@ export class AppController {
   getInfo() {
     return {
       publicKey: toBufferLE(this.rabinService.publicKey).toString('hex'),
+    };
+  }
+
+  @Get('/demo')
+  @ApiTags('demo')
+  @ApiOperation({ summary: 'how smart contract use this oracle' })
+  async demo() {
+    const host = 'https://oracle-demo.vercel.app';
+    // get oracle public key
+    const oraclePubKey = await axios
+      .get(`${host}/info`)
+      .then((r) => r.data)
+      .then((r) => PriceBet.parsePubKey(r));
+    console.log('oracle public key:', oraclePubKey);
+    // load contract
+    PriceBet.loadArtifact(artifact);
+    // create contract instance
+    const now = BigInt(Date.parse(new Date().toString()) / 1000);
+    const timestampFrom = now - 3600n;
+    const timestampTo = now + 3600n;
+    const trandingPair = toByteString('BSV-USDC', true);
+    const targetPrice = 10000n;
+    const decimal = 4n;
+    const myPubKey = myPublicKey;
+    const bobPubKey = bsv.PrivateKey.fromRandom(bsv.Networks.testnet).publicKey;
+    const instance = new PriceBet(
+      targetPrice,
+      decimal,
+      trandingPair,
+      timestampFrom,
+      timestampTo,
+      oraclePubKey,
+      PubKey(toHex(myPubKey)),
+      PubKey(toHex(bobPubKey)),
+    );
+    // connect contract instance to a testnet signer
+    const signer = new TestWallet(
+      myPrivateKey,
+      new DefaultProvider({ network: bsv.Networks.testnet }),
+    );
+    await instance.connect(signer);
+    // contract deploy
+    const deployTx = await instance.deploy();
+    console.log('deploy tx:', deployTx.id);
+    // get oracle price response
+    const priceResponse = await axios
+      .get(`${host}/v1/price/USDC/BSV`)
+      .then((r) => r.data);
+    const priceData = PriceBet.parseData(priceResponse);
+    const priceSig = PriceBet.parseSig(priceResponse);
+    // contract call
+    const winnerPubKey = myPubKey;
+    const { tx: callTx } = await instance.methods.unlock(
+      priceData,
+      priceSig,
+      (sigResps) => findSig(sigResps, winnerPubKey),
+      {
+        pubKeyOrAddrToSign: winnerPubKey,
+      } as MethodCallOptions<PriceBet>,
+    );
+    console.log('call tx:', callTx.id);
+    return {
+      deploy: deployTx.id,
+      call: callTx.id,
     };
   }
 }
